ecc679fae505594eefcc937fb8fef6fcbab41191
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-24 13:06:02 +0530

    Regex

---
+++ Regex/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ValidateEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please write an email to validate");
+                string sample = Console.ReadLine();
+                validateEmail validateEmail = new validateEmail();
+                if (sample == "")
+                    break;
+
+
+                Console.WriteLine(validateEmail.IsValidMail(sample));
+            }
+        }
+    }
+}
